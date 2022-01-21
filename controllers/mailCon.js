78e3188ed871adbc0d6fe277f7e29fb45e6f8dc2
jogeshgupta963 <jogeshgupta963@gmail.com>
2022-01-21 12:59:08 +0530

    changed git ignore

--- controllers/mailCon.js
+++ controllers/mailCon.js
@@ -23,32 +23,33 @@ function getSignup(req,res){
 async function postSignup(req,res){
     try {
         let userData = req.body;
+        
         let data = await mailModels.create(userData);
         
       
-
-// async..await is not allowed in global scope, must use a wrapper
-  async function main() {
+      
+      
+      async function main() {
   
   
-    let gmailPass = process.env.gmailPass
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    service: 'gmail',
-  auth: {
+        let gmailPass = process.env.gmailPass
+        
+         let transporter = nodemailer.createTransport({
+         service: 'gmail',
+         auth: {
     user: 'jogeshgupta963@gmail.com',
     pass: gmailPass
-    }
-  });
+           }
+        });
 
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: '"Mail 👻" <foo@example.com>', // sender address
-    to:data.email, // list of receivers
-    subject: "Hello ✔", // Subject line
-    text: `Hello ${data.name} `, // plain text body
-    html: `<b>HELLO ${data.name}</b>`, // html body
-  });
+       // send mail with defined transport object
+        let info = await transporter.sendMail({
+        from: '"Mail 👻" <foo@example.com>', // sender address
+         to:data.email, // list of receivers
+         subject: "Hello ✔", // Subject line
+         text: `Hello ${data.name} `, // plain text body
+           html: `<b>HELLO ${data.name}</b>`, // html body
+        });
 
   console.log("Message sent: %s", info.messageId);
   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
@@ -56,17 +57,19 @@ async function postSignup(req,res){
   // Preview only available when sending through an Ethereal account
   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
   // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-}
+      }
 
-main().catch(console.error);
+           main().catch(console.error);
 
-    console.log("Message sent: %s", info.messageId);
+          console.log("Message sent: %s", info.messageId);
+          
             res.json({message:"data inputted",dat:info.messageId});
-        
-        
-    } catch (err) {
-        res.send(err)
-    }
+            
+            
+          } catch (err) {
+            res.send(err)
+          }
+          
     
 }
 function redirectValid(req,res){
